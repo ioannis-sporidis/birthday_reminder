d3b3942c86719f8f13b60b5a053a5d8574421006
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-07-22 08:36:07 -0400

    Added functionality on List component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import { useState } from 'react';
+import data from './data';
 import List from './components/List';
 import Button from './components/Button';
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   const clickHandler = () => {
-    console.log('You clicked me!!!');
+    console.log(people);
   };
 
   return (
     <>
       <main>
         <section className='container'>
-          <h3>0 birthdays today</h3>
-          <List />
+          <h3>{people.length} birthdays today</h3>
+          <List people={people} />
           <Button onClick={clickHandler} type='button'>
             Clear All
           </Button>
--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,18 @@
-const List = () => {
+const List = ({ people }) => {
   return (
     <>
-      <h1>This is a list component</h1>
+      {people.map(person => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
